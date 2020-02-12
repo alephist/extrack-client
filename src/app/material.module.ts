4f5bff67399fcae08a0deace05f07217f9c37a30
Justin Necesito <justin.necesito@gmail.com>
2020-02-12 13:04:06 +0800

    Added initial styling to layout and navigation

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from "@angular/core";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
 
-const material = [MatToolbarModule, MatSidenavModule];
+const material = [
+  MatToolbarModule,
+  MatSidenavModule,
+  MatButtonModule,
+  MatIconModule,
+  MatListModule
+];
 
 @NgModule({
   imports: [material],
